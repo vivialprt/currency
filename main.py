b8869f0f31047b0c868983562c0363d28e49f8c9
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2021-07-14 18:08:16 +0300

    Added config and gitignore

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from datetime import timedelta
+import yaml
 from utils import collect_prices
 
 
 def main():
-    collect_prices(for_=timedelta(days=10), save_interval=timedelta(hours=1))
+    with open("config.cfg") as f:
+        cfg = yaml.load(f, yaml.SafeLoader)
+
+    period = timedelta(**cfg.get("CRAWLING_PERIOD", {"hours": 4}))
+    save_interval = timedelta(**cfg.get("SAVE_INTERVAL", {"minutes": 30}))
+    pair = cfg.get("PAIR", "BTC/USD")
+    log_level = cfg.get("LOG_LEVEL", "ERROR")
+
+    collect_prices(
+        for_=period,
+        pair=pair,
+        save_interval=save_interval,
+        log_level=log_level,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
-import time
 import os
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
-import numpy as np
 import pandas as pd
 from currencycom.client import Client
 
 
-def minute_passed(last_timestamp: datetime, current_timestamp: datetime) -> bool:
+def minute_passed(
+    last_timestamp: datetime, current_timestamp: datetime
+) -> bool:
     return current_timestamp - last_timestamp >= timedelta(minutes=1)
 
 
@@ -15,7 +15,9 @@ def hour_passed(last_timestamp: float, current_timestamp: datetime) -> bool:
     return current_timestamp - last_timestamp >= timedelta(hours=1)
 
 
-def delta_passed(last_timestamp:float, current_timestamp: datetime, delta: timedelta) -> bool:
+def delta_passed(
+    last_timestamp: float, current_timestamp: datetime, delta: timedelta
+) -> bool:
     return current_timestamp - last_timestamp >= delta
 
 
@@ -24,7 +26,7 @@ def time_up(current_timestamp: datetime, finish_time: datetime) -> bool:
 
 
 def get_server_time() -> datetime:
-    return datetimify(Client.get_server_time()['serverTime'])
+    return datetimify(Client.get_server_time()["serverTime"])
 
 
 def datetimify(time: str) -> datetime:
@@ -33,29 +35,37 @@ def datetimify(time: str) -> datetime:
 
 def collect_prices(
     for_: timedelta,
-    pair: str = 'BTC/USD',
-    save_interval: timedelta = timedelta(hours=1)
+    pair: str = "BTC/USD",
+    save_interval: timedelta = timedelta(hours=1),
+    log_level: str = 'ERROR',
 ) -> None:
+
+    def save_prices():
+        time_str = save_time.strftime("%y-%m-%d_%H-%M")
+        pair_str = "_".join(pair.split("/"))
+        filename = f'data/{pair_str}_{time_str}_prev{save_interval}.csv'
+        pd.DataFrame.from_records(prices).drop_duplicates().to_csv(filename)
+        print(f"Saved to {filename}")
 
     current_time = save_time = start_time = get_server_time()
     prices = []
-    print(f'Start time: {start_time}')
+    print(f"Start time: {start_time}")
 
     while not delta_passed(start_time, current_time, for_):
         try:
             current_time = get_server_time()
             price_change = Client.get_24h_price_change(pair)
-            prices.append({
-                'price': float(price_change['lastPrice']),
-                'time': datetimify(price_change['closeTime'])
-            })
+            prices.append(
+                {
+                    "price": float(price_change["lastPrice"]),
+                    "time": datetimify(price_change["closeTime"]),
+                }
+            )
 
             if delta_passed(save_time, current_time, save_interval):
-                print('delta passed')
+                print("delta passed")
                 save_time = current_time
-                filename = f'data/{"_".join(pair.split("/"))}_{save_time.strftime("%y-%m-%d_%H-%M")}_prev{save_interval}.csv'
-                pd.DataFrame.from_records(prices).drop_duplicates().to_csv(filename)
-                print(f'Saved to {filename}')
+                save_prices()
                 prices = []
         except Exception as e:
             print(e)
@@ -63,14 +73,23 @@ def collect_prices(
             break
 
     save_time = current_time
-    filename = f'data/{"_".join(pair.split("/"))}_{save_time.strftime("%y-%m-%d_%H-%M")}_prev{save_interval}.csv'
-    pd.DataFrame.from_records(prices).drop_duplicates().to_csv(filename)
-    print(f'Saved to {filename}')
+    save_prices()
 
 
-def load_prices(data_dir='data'):
+def load_prices(data_dir="data"):
     series = []
     for f in os.listdir(data_dir):
-        if f.endswith('csv'):
-            series.append(pd.read_csv(os.path.join(data_dir, f), index_col=0, parse_dates=['time']))
-    return pd.concat(series).drop_duplicates().sort_values('time').reset_index(drop=True)
+        if f.endswith("csv"):
+            series.append(
+                pd.read_csv(
+                    os.path.join(data_dir, f),
+                    index_col=0,
+                    parse_dates=["time"],
+                )
+            )
+    return (
+        pd.concat(series)
+        .drop_duplicates()
+        .sort_values("time")
+        .reset_index(drop=True)
+    )
